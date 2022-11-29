e33ebba44aac345746ee9f14d28c95f0c9579474
MarionBaussart <marion.baussart@gmail.com>
2022-11-29 11:38:35 +0000

    T2, matrix size for all

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -10,16 +10,7 @@ def matrix_shape(matrix):
             size of the matrix
     """
     size = []
-    for matrice in matrix:
-        nbmatrice = len(matrix)
-        for line in matrice:
-            if type(line) == list:
-                nbline = len(matrice)
-                nbcolumn = len(line)
-                size = [nbmatrice, nbline, nbcolumn]
-            else:
-                for line in matrix:
-                    nbline = len(matrix)
-                    nbcolumn = len(line)
-                    size = [nbline, nbcolumn]
+    while type(matrix) == list:
+        size.append(len(matrix))
+        matrix = matrix[0]
     return size
