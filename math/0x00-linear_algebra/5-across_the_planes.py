4a8ed9cffbbd16831c17b6d1af0d4c421178af65
MarionBaussart <marion.baussart@gmail.com>
2022-11-30 12:56:45 +0000

    update T5

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -9,7 +9,7 @@ def add_matrices2D(mat1, mat2):
         Return:
             the addition, None if matrices are of different shape
     """
-    if len(mat1[0]) == 0 or len(mat2[0]) == 0:
+    if len(mat1) == 0 or len(mat2) == 0:
         return None
     if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
         result = [[mat1[j][i] + mat2[j][i] for i in range(len(mat1))]
