6c8139ffc538ba920018735e9a835101462395a7
MarionBaussart <marion.baussart@gmail.com>
2022-12-06 17:25:24 +0000

    add tasks 0 to 17

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3trans
+"""script that calculates the integral of a polynomial"""
+
+
+def poly_integral(poly, C=0):
+    """function that calculates the integral of a polynomial
+        Args:
+            poly is a list of coefficients representing a polynomial
+            C is an integer representing the integration constant
+        Return:
+            a new list of coefficients representing the integral
+            of the polynomial
+    """
+    if type(poly) != list or type(C) != int and type(C) != float:
+        return None
+
+    integral = [C]
+    for i in range(len(poly)):
+        if (poly[i] / (i + 1)) % 1 == 0:
+            integral.append(int(poly[i] / (i + 1)))
+        else:
+            integral.append(poly[i] / (i + 1))
+    return integral
---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3trans
+"""script that calculate the sum of i²"""
+
+
+def summation_i_squared(n):
+    """function that calculate the sum of i²
+        Args:
+            n is the stopping condition
+        Return:
+            the integer value of the sum, None if n is not a valid number
+    """
+    if type(n) != int or n < 1:
+        return None
+
+    if n == 1:
+        return 1
+
+    return summation_i_squared(n - 1) + n ** 2
