c2481f4c90302798d97b93a20be51483a2d2f470
MarionBaussart <marion.baussart@gmail.com>
2022-12-07 13:42:14 +0000

    update T9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -9,13 +9,10 @@ def summation_i_squared(n):
         Return:
             the integer value of the sum, None if n is not a valid number
     """
-    if type(n) != int or n < 0:
+    if type(n) != int or n < 1:
         return None
 
     if n == 1:
         return 1
 
-    if n == 0:
-        return 0
-
-    return summation_i_squared(n - 1) + n ** 2
+    return int(summation_i_squared(n - 1) + n ** 2)
