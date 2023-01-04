09fa94b1902138d458758842d39bb4c31c743dcb
MarionBaussart <marion.baussart@gmail.com>
2023-01-04 13:50:57 +0000

    T14

--- supervised_learning/0x01-classification/14-neural_network.py
+++ supervised_learning/0x01-classification/14-neural_network.py
@@ -200,13 +200,13 @@ class NeuralNetwork:
             iterations: the number of iterations to train over
             alpha: the learning rate
         """
-        if type(iterations) is not int:
+        if type(iterations) != int:
             raise TypeError("iterations must be an integer")
-        if iterations < 1:
+        if iterations < 0:
             raise ValueError("iterations must be a positive integer")
-        if type(alpha) is not float:
+        if type(alpha) != float:
             raise TypeError("alpha must be a float")
-        if alpha <= 0:
+        if alpha < 0:
             raise ValueError("alpha must be positive")
 
         for i in range(iterations):
