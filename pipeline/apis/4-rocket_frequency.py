3b592729a3d8b79f95e99383714992715f22df16
MarionBaussart <marion@Michelle>
2024-03-27 23:19:02 +0100

    update T4, url V4 and rocket

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -8,12 +8,20 @@ import requests
 
 if __name__ == '__main__':
 
-    url = 'https://api.spacexdata.com/v4/launches'
-    res = requests.get(url).json()
+    url_launch = 'https://api.spacexdata.com/v4/launches'
+    url_rocket = 'https://api.spacexdata.com/v4/rockets'
+    res_launch = requests.get(url_launch).json()
+    res_rocket = requests.get(url_rocket).json()
+
     rockets = {}
 
-    for launch in res:
-        rocket_name = launch['rocket']['rocket_name']
+    for launch in res_launch:
+        rocket_id = launch['rocket']
+
+        for rocket in res_rocket:
+            if rocket['id'] == rocket_id:
+                rocket_name = rocket['name']
+
         if rocket_name in rockets:
             rockets[rocket_name] += 1
         else:
